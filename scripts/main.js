96e15a42104581aefc1d7d3200550e8fdee1c0a1
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-07-12 22:19:20 -0500

    added first component

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
 
+/*
+    - Store Picker
+    - render says "what HTML do you want me to show?"
+    - this code below will let me make <StorePicker />
+*/
+
+var StorePicker = React.createClass({
+
+    render: function() {
+        return (
+            <p>hello react</p>
+        )
+    }
+
+})
+
+
+
+ReactDOM.render(<StorePicker/>, document.getElementById('main'))
