ac387f0a860f1d6889489696fc8013fd6cdefa4c
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-07-12 23:07:34 -0500

    set up all the component structures....no data

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,73 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
+
+/*
+    App
+*/
+
+var App = React.createClass({
+
+    render: function() {
+        return (
+            <div className="catch-of-the-day">
+                <div className ="menu">
+                    <Header/>
+                </div>
+                <Order/>
+                <Inventory/>
+            </div>
+        )
+    }
+})
+
+
+
+/*
+    Header
+    <Header/>
+*/
+
+var Header = React.createClass({
+
+    render: function() {
+        return (
+            <p>Header</p>
+        )
+    }
+})
+
+
+/*
+    Order
+    <Order/>
+*/
+
+var Order = React.createClass({
+
+    render: function() {
+        return (
+            <p>Order</p>
+        )
+    }
+})
+
+
+/*
+    Inventory
+    <Inventory/>
+*/
+
+var Inventory = React.createClass({
+
+    render: function() {
+        return (
+            <p>Inventory</p>
+        )
+    }
+})
+
+
 /*
     - Store Picker
     - render says "what HTML do you want me to show?"
@@ -16,7 +83,7 @@ var StorePicker = React.createClass({
     var name = 'beez'
         return (
             <form className="store-selector">
-                {/* this is how you write comments in JSX*/}
+                 {/* this is how you write comments in JSX*/}
                 <h2>Please Enter A Store {name}</h2>
                 <input type="text" ref="storeId" required />
                 <input type="submit" />
@@ -28,4 +95,5 @@ var StorePicker = React.createClass({
 
 
 
-ReactDOM.render(<StorePicker/>, document.getElementById('main'))
+ReactDOM.render(<App/>, document.querySelector('#main'))
+
