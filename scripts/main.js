d7b347e39576bb9babdda36f092f74e57a158b82
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-07-12 22:44:05 -0500

    added a form

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,22 @@ var ReactDOM = require('react-dom')
     - Store Picker
     - render says "what HTML do you want me to show?"
     - this code below will let me make <StorePicker />
+    - render can only return one thing. like a form or a div but not multiple p tags
+    - MUST HAVE self closing tags
 */
 
 var StorePicker = React.createClass({
 
     render: function() {
+
+    var name = 'beez'
         return (
-            <p>hello react</p>
+            <form className="store-selector">
+                {/* this is how you write comments in JSX*/}
+                <h2>Please Enter A Store {name}</h2>
+                <input type="text" ref="storeId" required />
+                <input type="submit" />
+            </form>
         )
     }
 
