4afec4bc91473c3d041773d12ac1e8ab00227933
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-08-04 22:56:02 -0500

    okay finally got the fish uploading to firebase. problem was in the base.syncState(). had to put some specs inside of there

--- scripts/main.js
+++ scripts/main.js
@@ -64,7 +64,11 @@ var App = React.createClass({
         console.log('component did mount')
 
         // base is ref to firebase. syncState connects our React state (fishes, order) to the one in Firebase. so all this happens on page load because of componentDidMount
-        base.syncState() // this is the part that's giving me issues
+
+        base.syncState(this.props.params.storeId + '/fishes', {
+            context : this,
+            state : 'fishes'
+        })
     },
     addToOrder: function(key) {
         // ** we had to use [key] notation instead of .key because key is a variable ???
